e00cdbedc2e5f88c2c4cd73591d36480f0e616dd
Thomas AFONSO <72008806+Thomas-Af@users.noreply.github.com>
2021-02-23 16:13:18 +0100

    map monde

---
+++ script.js
@@ -0,0 +1,165 @@
+var agroenland = document.getElementById('groenland');
+var overlaygroenland = document.getElementById('overlay-groenland');
+agroenland.addEventListener('click',openModalgroenland);
+function openModalgroenland() {
+overlaygroenland.style.display='block';
+}
+
+var closegroenland = document.getElementById('close-groenland');
+closegroenland.addEventListener('click',closeModalgroenland);
+function closeModalgroenland() {
+overlaygroenland.style.display='none';
+}
+/*----------------------------------------------------------*/
+var aaustralie = document.getElementById('australie');
+var overlayaustralie = document.getElementById('overlay-australie');
+aaustralie.addEventListener('click',openModalaustralie);
+function openModalaustralie() {
+overlayaustralie.style.display='block';
+}
+
+var closeaustralie = document.getElementById('close-australie');
+closeaustralie.addEventListener('click',closeModalaustralie);
+function closeModalaustralie() {
+overlayaustralie.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var aargentine = document.getElementById('argentine');
+var overlayargentine = document.getElementById('overlay-argentine');
+aargentine.addEventListener('click',openModalargentine);
+function openModalargentine() {
+overlayargentine.style.display='block';
+}
+
+var closeargentine = document.getElementById('close-argentine');
+closeargentine.addEventListener('click',closeModalargentine);
+function closeModalargentine() {
+overlayargentine.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var abresil = document.getElementById('bresil');
+var overlaybresil = document.getElementById('overlay-bresil');
+abresil.addEventListener('click',openModalbresil);
+function openModalbresil() {
+overlaybresil.style.display='block';
+}
+
+var closebresil = document.getElementById('close-bresil');
+closebresil.addEventListener('click',closeModalbresil);
+function closeModalbresil() {
+overlaybresil.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var acanada = document.getElementById('canada');
+var overlaycanada = document.getElementById('overlay-canada');
+acanada.addEventListener('click',openModalcanada);
+function openModalcanada() {
+overlaycanada.style.display='block';
+}
+
+var closecanada = document.getElementById('close-canada');
+closecanada.addEventListener('click',closeModalcanada);
+function closeModalcanada() {
+overlaycanada.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var achine = document.getElementById('chine');
+var overlaychine = document.getElementById('overlay-chine');
+achine.addEventListener('click',openModalchine);
+function openModalchine() {
+overlaychine.style.display='block';
+}
+
+var closechine = document.getElementById('close-chine');
+closechine.addEventListener('click',closeModalchine);
+function closeModalchine() {
+overlaychine.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var aallemagne = document.getElementById('allemagne');
+var overlayallemagne = document.getElementById('overlay-allemagne');
+aallemagne.addEventListener('click',openModalallemagne);
+function openModalallemagne() {
+overlayallemagne.style.display='block';
+}
+
+var closeallemagne = document.getElementById('close-allemagne');
+closeallemagne.addEventListener('click',closeModalallemagne);
+function closeModalallemagne() {
+overlayallemagne.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var ainde = document.getElementById('inde');
+var overlayinde = document.getElementById('overlay-inde');
+ainde.addEventListener('click',openModalinde);
+function openModalinde() {
+overlayinde.style.display='block';
+}
+
+var closeinde = document.getElementById('close-inde');
+closeinde.addEventListener('click',closeModalinde);
+function closeModalinde() {
+overlayinde.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var aroyaumeuni = document.getElementById('royaumeuni');
+var overlayroyaumeuni = document.getElementById('overlay-royaumeuni');
+aroyaumeuni.addEventListener('click',openModalroyaumeuni);
+function openModalroyaumeuni() {
+overlayroyaumeuni.style.display='block';
+}
+
+var closeroyaumeuni = document.getElementById('close-royaumeuni');
+closeroyaumeuni.addEventListener('click',closeModalroyaumeuni);
+function closeModalroyaumeuni() {
+overlayroyaumeuni.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var arussie = document.getElementById('russie');
+var overlayrussie = document.getElementById('overlay-russie');
+arussie.addEventListener('click',openModalrussie);
+function openModalrussie() {
+overlayrussie.style.display='block';
+}
+
+var closerussie = document.getElementById('close-russie');
+closerussie.addEventListener('click',closeModalrussie);
+function closeModalrussie() {
+overlayrussie.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var afrance = document.getElementById('france');
+var overlayfrance = document.getElementById('overlay-france');
+afrance.addEventListener('click',openModalfrance);
+function openModalfrance() {
+overlayfrance.style.display='block';
+}
+
+var closefrance = document.getElementById('close-france');
+closefrance.addEventListener('click',closeModalfrance);
+function closeModalfrance() {
+overlayfrance.style.display='none';
+}
+
+/*----------------------------------------------------------*/
+var ausa = document.getElementById('usa');
+var overlayusa = document.getElementById('overlay-usa');
+ausa.addEventListener('click',openModalusa);
+function openModalusa() {
+overlayusa.style.display='block';
+}
+
+var closeusa = document.getElementById('close-usa');
+closeusa.addEventListener('click',closeModalusa);
+function closeModalusa() {
+overlayusa.style.display='none';
+}
